452432e529b4cbc531e8dda32936c250071bdfef
Samuel Sharp <samuelsharp4@gmail.com>
2020-06-28 19:25:50 +0000

    Get User Location & Display Weather.

--- public/getdata.js
+++ public/getdata.js
@@ -1,18 +1,20 @@
-function star(x) {
-	x.classList.toggle("fas");
-	var favref = db.collection(uid).doc("favorites");
-	favref.set({ [document.querySelector('#nameTitle').value]: true }, { merge: true });
-	alert("This crop has been added to your favorites!")
-}
-
 function getData() {
-	fetch('https://cors-anywhere.herokuapp.com/api.weatherstack.com/current?access_key=dc9db6d6e2d05fd73bf8893f6c88c0fc&query=San+Diego').then(function (response) {
-		return response.json();
-	}).then(function (obj) {
-		document.getElementById("temperature").innerHTML = obj.current.temperature + "C";
+	if ('geolocation' in navigator) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			console.log(position.coords);
+			fetch('https://cors-anywhere.herokuapp.com/api.weatherstack.com/current?access_key=dc9db6d6e2d05fd73bf8893f6c88c0fc&query=' + position.coords.latitude + "," + position.coords.longitude).then(function (response) {
+				return response.json();
+			}).then(function (obj) {
+				document.getElementById("temperature").innerHTML = obj.current.temperature + "C";
+				document.getElementById("uv").innerHTML = "UV Index: " + obj.current.uv_index;
+				document.getElementById("precipitation").innerHTML = "Precipitation chance: " + obj.current.precip;
+			}).catch(function (error) {
+				console.error(error);
+			});
+		});
+	} else {
+		document.getElementById("temperature").innerHTML = "Enable Device Location to get the weather.";
 		document.getElementById("uv").innerHTML = "UV Index: " + obj.current.uv_index;
 		document.getElementById("precipitation").innerHTML = "Precipitation chance: " + obj.current.precip;
-	}).catch(function (error) {
-		console.error(error);
-	});
+	}
 }
